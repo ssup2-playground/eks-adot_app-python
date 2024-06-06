dd91929ab718498c57d261bac38e3de04da34d03
ssup2 <supsup5642@gmail.com>
2024-06-06 14:04:07 +0900

    add ping apis and logging

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
+import logging
+import time
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-# App handlers
-@app.get("/hello")
-async def root():
-    return {"message": "Hello"}
+# Ping handlers
+@app.get("/ping")
+async def get_ping():
+    logging.info("pong")
+    return {"message": "pong"}
+
+@app.get("/pingsleep5s")
+async def get_pingsleep5():
+    time.sleep(5)
+    logging.info("pong sleep 5s")
+    return {"message": "pong sleep 5s"}
+
+@app.get("/pingsleep10s")
+async def get_pingsleep10():
+    time.sleep(10)
+    logging.info("pong sleep 10s")
+    return {"message": "pong sleep 10s"}
 
 # Health handlers
 @app.get("/healthz")
 def get_heatlhz() -> dict[str, str]:
-    return {"status": "UP"}
+    logging.info("healthz")
+    return {"status": "up"}
